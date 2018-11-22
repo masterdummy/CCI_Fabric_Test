3a2fe33fee3c10a0af107b2222c7f0c28ffb1904
Simone Barbara <simone.barbara@toyotaconnected.eu>
2018-11-22 10:45:30 +0000

    Initial Commit

---
+++ CCI_Fabric_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CCI_Fabric_Test
+//
+//  Created by Simone Barbara B on 22/11/2018.
+//  Copyright © 2018 Simone Barbara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CCI_Fabric_TestTests/CCI_Fabric_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  CCI_Fabric_TestTests.swift
+//  CCI_Fabric_TestTests
+//
+//  Created by Simone Barbara B on 22/11/2018.
+//  Copyright © 2018 Simone Barbara. All rights reserved.
+//
+
+import XCTest
+@testable import CCI_Fabric_Test
+
+class CCI_Fabric_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
