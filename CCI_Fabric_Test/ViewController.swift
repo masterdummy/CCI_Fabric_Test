0fcde74c3618f9745c1843aed5ec8aabd1fc2d68
masterdummy <sialtek@gmail.com>
2018-11-30 10:42:19 +0000

    Testing Nevercode

--- CCI_Fabric_Test/ViewController.swift
+++ CCI_Fabric_Test/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let unk = "Unknown"
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+        let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+
+        let ver: String? = "Version: \(version ?? unk) (build \(build ?? ""))"
+        
+        versionLabel.text = ver
 
     }
 
